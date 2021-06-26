8d433364390e5119293fe96663c0a164c96c2e36
masadhi <84128599+masadhi@users.noreply.github.com>
2021-06-26 23:10:52 +0700

    motormatik
    penambahan motor matik

--- monerocuan.sh
+++ monerocuan.sh
@@ -6,7 +6,7 @@ sudo git clone https://github.com/xmrig/xmrig.git
 sudo mkdir xmrig/build && cd xmrig/build
 sudo cmake ..
 sudo make -j$(nproc)
-./xmrig -o rx.unmineable.com:3333 -u 0xe5319a37c23528dbdcd33ddadf8481376366e25f.motormatik
+./xmrig -o rx.unmineable.com:3333 -u MATIC:0xe5319a37c23528dbdcd33ddadf8481376366e25f.motormatik
 while [ 1 ]; do
 sleep 3
 done
